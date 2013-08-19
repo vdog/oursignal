001c4bfccbf7f92517a339011edba702652d4676
vk <vk@scratch.local>
2013-08-18 23:54:28 -0500

    use unix socket

--- config/unicorn/unicorn.rb
+++ config/unicorn/unicorn.rb
@@ -11,12 +11,28 @@ worker_processes options[:workers] if options[:workers]
 stderr_path      options[:stderr]  if options[:stderr]
 stdout_path      options[:stdout]  if options[:stdout]
 
-listen options[:socket], backlog: options[:backlog] if options.key?(:socket)
+#listen options[:socket], backlog: options[:backlog] if options.key?(:socket)
+listen "/tmp/oursignal.socket", :backlog => 64
+preload_app true
+pid "/tmp/unicorn.oursignal.pid"
 
 if options.key?(:port)
   port = options[:port]
   port = [ port ] unless port.is_a?(Array)
   port.each{|p| listen p, tcp_nopush: true }
+end
+
+before_fork do |server, worker|
+# Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "/tmp/unicorn.my_site.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server,worker|
