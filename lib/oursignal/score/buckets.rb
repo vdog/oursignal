bc52cdacb76052724d83ed27b7e33c0ce80b68d2
vk <vk@scratch.local>
2013-08-19 03:51:46 -0500

    Get the fricking thing to update

--- lib/oursignal/score/buckets.rb
+++ lib/oursignal/score/buckets.rb
@@ -6,6 +6,7 @@ module Oursignal
     module Buckets
       def self.perform
         Oursignal.db.transaction do
+	#	raise "Vince wants to stop here"
           Oursignal.db.execute('delete from score_buckets') # Last bucket run.
           Score.sources.each do |source|
             total, max = Oursignal.db.execute("select count(*) as total, max(#{source}) from links where #{source} > 0").first.values_at(:total, :max)
@@ -15,7 +16,15 @@ module Oursignal
             buckets  = (0..99).map do |offset|
               score_st.execute((total * (offset.to_f / 100)).to_i).first[:score].to_f
             end
-            ScoreBucket.create(source: source, buckets: buckets)
+		puts source
+		str_buckets = '{' + buckets[0].to_s
+		(1..99).each do |value|
+			str_buckets += ", " + buckets[value].to_s 
+		end
+		str_buckets += '}'
+		puts str_buckets
+            #ScoreBucket.create(source: source, buckets: buckets)
+            ScoreBucket.create(source: source, buckets: str_buckets)
           end
         end
       end
--- lib/oursignal/score/timestep.rb
+++ lib/oursignal/score/timestep.rb
@@ -35,10 +35,11 @@ module Oursignal
             from links
             where referred_at > now() - interval '#{LIFETIME} seconds'
           })
-          puts "selected %d links in\t%10.4fs" % [links.count, Time.now - now]
+          puts "HELLO!!! selected %d links in\t%10.4fs" % [links.count, Time.now - now]
           raise "Timestep requires a minimum of 100 links in the DB but only #{links.count} exist." unless links.count >= 100
 
           buckets = Score.sources.inject({}) do |buckets, source|
+		puts source
             sb = ScoreBucket.find(source)
             buckets[source] = sb if sb
             buckets
@@ -49,11 +50,13 @@ module Oursignal
           scores_sth = Oursignal.db.prepare('select * from scores where link_id = ? and bucket > 0 order by timestep_id desc')
           links.each_with_index do |link, index|
             scores_ts += 1
+		puts "Injecting..."
             bucket_scores = Score.sources.inject([]) do |acc, source|
               acc << buckets[source].at(link[source]) if buckets[source]
               acc
             end.select{|score| score > 0}
             next if bucket_scores.empty?
+		puts "bucket_scores not empty"
 
             bucket_average = bucket_scores.inject(:+) / bucket_scores.size
             bucket_bonus   = bucket_scores.size.to_f / Score.sources.size
@@ -75,6 +78,7 @@ module Oursignal
             # score = [bucket - (link.delete(:seconds).to_i * decay), 0].max
 
             # Add a score.
+		puts "Creating score for timescep_id: %d, bucket: %s, link: %s" % [step.id, bucket, link]
             Score.create({timestep_id: step.id, score: score, ema: ema, bucket: bucket}.merge(link))
           end
           puts "created %d scores in\t%10.4fs" % [scores_ts, Time.now - now]
